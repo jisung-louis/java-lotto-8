7405145fc06840addb761d8eec533cbda631106c
전지성 <jisung@jeonjiseong-ui-MacBookAir.local>
2025-11-02 22:09:32 +0900

    feat(domain): 로또 발행 및 출력 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,11 +2,14 @@ package lotto;
 
 import lotto.domain.LottoStore;
 import lotto.view.InputView;
+import lotto.view.OutputView;
+
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
         int purchaseAmount = InputView.readPurchaseAmount();
-        int count = LottoStore.calculateCount(purchaseAmount);
-        System.out.println(count + "개를 구매했습니다.");
+        List<Lotto> lottos = LottoStore.buyLottos(purchaseAmount);
+        OutputView.printLottos(lottos);
     }
 }
--- src/main/java/lotto/domain/LottoStore.java
+++ src/main/java/lotto/domain/LottoStore.java
@@ -1,9 +1,25 @@
 package lotto.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import lotto.Lotto;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class LottoStore {
     private static final int LOTTO_PRICE = 1000;
 
-    public static int calculateCount(int purchaseAmount) {
-        return purchaseAmount / LOTTO_PRICE;
+    public static List<Lotto> buyLottos(int purchaseAmount) {
+        int count = purchaseAmount / LOTTO_PRICE;
+        List<Lotto> lottos = new ArrayList<>();
+
+        for (int i = 0; i < count; i++) {
+            List<Integer> numbers = new ArrayList<>(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+            Collections.sort(numbers);
+            lottos.add(new Lotto(numbers));
+        }
+
+        return lottos;
     }
 }
